f6369610802b48d56a2fc98e7d7dfa7c51b08e54
Sandeep Khadka <sandeepkhadka4935g@gmail.com>
2022-09-05 13:37:33 +0545

    Parent Tables created by Sandeep

---
+++ database/migrations/2022_09_04_121723_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content')->nullable();
+            $table->enum('status', ['Active', 'Inactive'])->default('Inactive');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+};
