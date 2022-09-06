3e6d5b9012bb36902dde072c909a03c0be9f2ed1
Sandeep Khadka <sandeepkhadka4935g@gmail.com>
2022-09-06 17:39:52 +0545

    Package Controller created by Sandeep

--- resources/views/admin/Packages/package/packageList.blade.php
+++ resources/views/admin/Packages/package/packageList.blade.php
@@ -6,45 +6,66 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title" style="margin-top: 8px">Packages</h3>
-                        <a href="{{ url('packageForm') }}" class="btn btn-primary" style="margin-left:949px;";>
-                            Add
+                        <h3 class="card-title" style="margin-top: 8px; font-weight: bold">Packages</h3>
+                        <a href="{{ url('packageForm') }}" class="btn btn-success float-right" style="margin-bottom: 0px"><i
+                                class="fa fa-plus" style="font-size: 12px">
+                                Add Package
+                            </i>
                         </a>
                         {{-- <button href="{{url('packageList')}}" class='btn btn-primary' style="margin-left: 949px">Add</button> --}}
                     </div>
-
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <table id= "searching" class="table table-bordered">
+                        <table id="searching" class="table table-bordered">
                             <thead>
                                 <tr>
-                                    <th style="width: 10px">P.N.</th>
-                                    <th>Category(id)</th>
+                                    <th style="width: 10px">S.N.</th>
                                     <th>Name</th>
+                                    <th>Category</th>
                                     <th>Price</th>
                                     <th style="width: 90px">Status</th>
-                                    <th style="width: 150px">Action</th>
+                                    <th style="width: 190px">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>1.</td>
-                                    <td>Update software</td>
-                                    <td>
-                                        <div class="progress progress-xs">
-                                            <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                        </div>
-                                    </td>
-                                    <td><span class="badge bg-danger">55%</span></td>
-                                    <td><span class="badge bg-success">Active</span></td>
-                                    <td><a href="#"><button class="btn btn-danger" style="margin-right: 10px"><i
-                                                    class="fa fa-trash"></button></i></a>
-                                        <a href="{{ url('packageView') }}"><button class="btn btn-primary" style="margin-right: 10px"><i
-                                                    class="fa fa-eye"></button></i></a>
-                                        <a href="{{ url('packageForm') }}"><button class="btn btn-success"style="margin-right: 10px"><i
-                                                    class="fa fa-pencil"></button></i></a>
-                                    </td>
-                                </tr>
+                                @if (isset($package_data))
+                                    @foreach ($package_data as $package)
+                                        <tr>
+                                            <td>1</td>
+                                            <td>{{ $package->name }}</td>
+                                            <td>
+                                                @if (isset($package->category_info['title']))
+                                                    {{ $package->category_info['title'] }}
+                                                @else
+                                                    --
+                                                @endif
+                                                {{-- {{($package->category_info['title']) == null ? '' : $package->category_info['title']}} --}}
+                                            </td>
+                                            <td>${{ $package->price }}</td>
+                                            <td>{{ ucfirst($package->status) }}</td>
+                                            <td>
+                                                <a href="{{ route('package.show', $package->id) }}" class="btn btn-primary">
+                                                    <i class="fa fa-eye">
+
+                                                    </i>
+                                                </a>
+                                                <a href="{{ route('package.edit', $package->id) }}" class="btn btn-success">
+                                                    <i class="fa fa-pen">
+
+                                                    </i>
+                                                </a>
+                                                <form action="{{ route('package.destroy', $package->id) }}" method="post"
+                                                    class="d-inline">
+                                                    @method('delete')
+                                                    @csrf
+                                                    <button class="btn btn-danger"
+                                                        onclick="return confirm('Do you want to delete this package?');"><i
+                                                            class="fa fa-trash"></i></button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
 
                             </tbody>
                         </table>
