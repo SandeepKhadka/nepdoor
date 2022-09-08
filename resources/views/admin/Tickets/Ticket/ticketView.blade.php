35058c1eddb93f2ba260c315db116a0e34b14baa
unknown <knetexbhandari2002@gmail.com>
2022-09-08 16:07:29 +0545

    Edited ticket form by sandeep

--- resources/views/admin/Tickets/Ticket/ticketView.blade.php
+++ resources/views/admin/Tickets/Ticket/ticketView.blade.php
@@ -4,69 +4,109 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h4 class="m-0 text-left font-weight-bold" style="padding: 10px">Ticket Form</small></h4>
+                    <h4 class="m-0 text-left font-weight-bold" style="padding: 10px">Ticket
+                        {{ isset($ticket_data) ? 'Update' : 'Add' }}</small></h4>
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
                                 <div class="form-group col-md-12">
-                                    <label for="token_id">Token</label>
-                                    <input type="text" id="token_id" name="token_id" class="form-control" value="{{ @$ticket_data->token_id }}" disabled>
-                                   
+                                    <label for="title">Title</label>
+                                    <input type="text" id="title" name="title" class="form-control"
+                                        value="{{ @$ticket_data->title }}" disabled>
+                                    @error('title')
+                                        <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
-
                             </div>
                             <div class="row">
                                 <div class="form-group col-md-6">
-                                    <label for="subs_id">Subscription </label>
-                                    <input type="text" id="subs_id" name="subs_id" class="form-control" value="{{ @$ticket_data->subs_id }}" disabled>
-                                   
+                                    <label for="subs_id">Subscription</label>
+                                    <select type="text" class="form-control form-control-sm" id="subs_id"
+                                        name="subs_id" disabled>
+                                        <option></option>
+                                    </select>
+                                    @error('subs_id')
+                                        <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="token_id">Token ID</label>
+                                    <input type="text" id="token_id" name="token_id" class="form-control"
+                                        value="{{ @$ticket_data->token_id }}" disabled>
+                                    @error('token_id')
+                                        <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
 
-                                <div class="form-group col-md-6">
-                                    <label for="title">Title</label>
-                                    <input type="text" id="title" name="title" class="form-control" value="{{ @$ticket_data->title }}" disabled>
-                                   
-                                </div>
                             </div>
 
                             <div class="row">
                                 <div class="form-group col-md-12">
                                     <label for="priority">Priority</label>
-                                    <input type="text" id="priority" name="priority" class="form-control" value="{{ @$ticket_data->priority }}" disabled>
-                                    
+                                    <select type="text" class="form-control form-control-sm" id="priority"
+                                        name="priority" disabled>
+                                        <option {{ @$ticket_data->priority == 'Normal' ? 'selected' : '' }}>Normal
+                                        </option>
+                                        <option {{ @$ticket_data->priority == 'Urgent' ? 'selected' : '' }}>Urgent
+                                        </option>
+                                    </select>
+                                    @error('priority')
+                                        <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
+
 
                             <div class="row">
                                 <div class="form-group col-md-12">
                                     <label for="message">Message</label>
-                                    <textarea type="text" id="message" name="message" class="form-control" rows="5" disabled style="resize:none;">{{ @$ticket_data->message }}</textarea>
-                                   
+                                    <textarea type="text" id="message" name="message" class="form-control" rows="5" disabled
+                                        style="resize: none;">{{ @$ticket_data->message }}</textarea>
+                                    @error('message')
+                                        <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
-
                             <div class="row">
                                 <div class="form-group col-md-6">
                                     <label for="ticket_status">Ticket Status</label>
-                                    <input type="text" id="ticket_status" name="ticket_status" class="form-control" value="{{ @$ticket_data->ticket_status }}" disabled>
-                                    
+                                    <select type="text" class="form-control form-control-sm" id="ticket_status"
+                                        name="ticket_status" disabled>
+                                        <option {{ @$ticket_data->ticket_status == 'Opened' ? 'selected' : '' }}>
+                                            Opened</option>
+                                        <option {{ @$ticket_data->ticket_status == 'Closed' ? 'selected' : '' }}>Closed
+                                        </option>
+                                    </select>
+                                    @error('ticket_status')
+                                        <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
 
                                 <div class="form-group col-md-6">
                                     <label for="status">Status</label>
-                                    <input type="text" id="status" name="status" class="form-control" value="{{ @$ticket_data->status }}" disabled>
-                                   
+                                    <select type="text" class="form-control form-control-sm" id="status"
+                                        name="status" disabled>
+                                        <option {{ @$ticket_data->status == 'Active' ? 'selected' : '' }}>Active
+                                        </option>
+                                        <option {{ @$ticket_data->status == 'Inactive' ? 'selected' : '' }}>Inactive
+                                        </option>
+                                    </select>
+                                    @error('status')
+                                        <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
+                            <button type="submit" class="btn btn-success float-right" value="Sumbit">Submit</button>
                             <a href="{{ route('ticket.index') }}" class="btn btn-primary float-right"
                                 style="margin-right: 10px" value="Back">Back
                             </a>
+                            </form>
+                            </form>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
     </div>
 @endsection
